ca16cd2aad1bad9b5cbb03359b9c8a1043892dc1
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2024-08-20 11:59:43 -0300

    se instala @types/node

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+
+// Obtener el equivalente a __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
